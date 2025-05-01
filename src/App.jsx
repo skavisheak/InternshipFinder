2f8cd5d9dde9cc72cac7a060bd15806443130313
skavisheak <cst22065@std.uwu.ac.lk>
2025-05-01 22:38:03 +0530

    cards.jsx and navbar.jsx files are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
+import Navbar from "./Components/Navbar"
 
 
 function App() {
 
 
   return (
-    <>
-      <div className="mt-10 text-3xl text-center text-green-600">
-      Hello Tailwind + React!
-    </div>
-    </>
+    <main className="min-h-screen text-white bg-gray-100">
+      <Navbar/>
+    </main>
   )
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,23 @@
 function Navbar(){
 return(
     <>
-    <h1 className="mt-5 text-white"></h1>
+    <div className="p-2 pt-6">   
+        <div className="w-full text-center h-80 bg-gradient-to-b from-blue-900 to-blue-400 ">
+            <h1 className="text-5xl font-bold pt-7">
+                Student Internships Hub!
+            </h1>
+            <p className="pt-6">
+                Discover internship opportunities and post openings to connect students with real-world experience
+            </p>
+            <div className="pt-10 md:pt-20 sm:pt-16">
+                 <button className="px-5 py-2 mx-3 text-xl font-semibold text-black transition duration-200 bg-white rounded-md font-normalshadow hover:bg-gray-300 ">
+                 Get Started
+                 </button>
+
+            </div>
+        </div>
+   </div>
+
     </>
 )
 }

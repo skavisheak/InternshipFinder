8b89599cef6ce20a040e465a916ce725e7e8d5fd
skavisheak <cst22065@std.uwu.ac.lk>
2025-05-01 20:26:54 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className="mt-10 text-3xl text-center text-green-600">
+      Hello Tailwind + React!
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+
+function Navbar(){
+return(
+    <>
+    <h1 className="mt-5 text-white"></h1>
+    </>
+)
+}
+export default Navbar
